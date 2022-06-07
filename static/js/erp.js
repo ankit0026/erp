983bf4a73c8597fc826d740678c851b262d279a7
ankit0026 <ankit.chaudhary.cse.2020@miet.ac.in>
2022-06-07 20:37:46 +0530

    Update website

---
+++ static/js/erp.js
@@ -0,0 +1,42 @@
+function details1_show(){
+    document.getElementById("details1").style.display="block";
+    document.getElementById("details2").style.display="none";
+    document.getElementById("details3").style.display="none";
+    document.getElementById("details4").style.display="none";
+    document.getElementById("details5").style.display="none";
+}
+function details2_show(){
+    document.getElementById("details2").style.display="block";
+    document.getElementById("details1").style.display="none";
+    document.getElementById("details3").style.display="none";
+    document.getElementById("details4").style.display="none";
+    document.getElementById("details5").style.display="none";
+}
+function details3_show(){
+    document.getElementById("details3").style.display="block";
+    document.getElementById("details2").style.display="none";
+    document.getElementById("details1").style.display="none";
+    document.getElementById("details4").style.display="none";
+    document.getElementById("details5").style.display="none";
+}
+function details4_show(){
+    document.getElementById("details4").style.display="block";
+    document.getElementById("details2").style.display="none";
+    document.getElementById("details3").style.display="none";
+    document.getElementById("details1").style.display="none";
+    document.getElementById("details5").style.display="none";
+}
+function details5_show(){
+    document.getElementById("details5").style.display="block";
+    document.getElementById("details2").style.display="none";
+    document.getElementById("details3").style.display="none";
+    document.getElementById("details4").style.display="none";
+    document.getElementById("details1").style.display="none";
+}
+function goback(){
+    document.getElementById("details5").style.display="none";
+    document.getElementById("details2").style.display="none";
+    document.getElementById("details3").style.display="none";
+    document.getElementById("details4").style.display="none";
+    document.getElementById("details1").style.display="none";
+}
